81d6240d93976eb82de4e2b70177a6213aeb700d
Norbert <norbi-95@o2.pl>
2018-04-19 17:19:28 +0200

    added own mazes

--- www/js/main.js
+++ www/js/main.js
@@ -134,7 +134,7 @@ function navEvent(o)
 			changeMainScreenTo("P13");
 			setControlListeners();
 			}, 2000);
-			game.initLvl(game.curLvl);
+			game.initLvl(7);
 			break;
 		case 'P07_boton_volver':
 			changeMainScreenTo("P01");
